7c1256b0b9e9ec663faf50c453fea5e31bcef44d
Andrii-Ishchenko <andrey.ischencko@gmail.com>
2017-11-16 16:48:54 +0200

    fixed el on selected product

--- shop/src/app/products/selected-product/selected-product.component.ts
+++ shop/src/app/products/selected-product/selected-product.component.ts
@@ -1,44 +1,36 @@
-import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild, OnDestroy, AfterViewInit, AfterViewChecked, ChangeDetectorRef  } from '@angular/core';
 import { Product } from '../product';
 import { ImageService } from '../../image/image.service';
 import { ProductService } from '../product.service';
 import { ProductCommunicationService } from '../product-communication.service';
 import { Subscription } from 'rxjs/Subscription';
-import { OnDestroy, AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'selected-product',
   templateUrl: './selected-product.component.html',
   styleUrls: ['./selected-product.component.css']
 })
-export class SelectedProductComponent implements OnInit, OnDestroy, AfterViewInit {
+export class SelectedProductComponent implements OnInit, OnDestroy {
 
-	@ViewChild('image') 
-	el:ElementRef;
+	@ViewChild('img') 
+	el;
 
-	source: string;
 	private subscription : Subscription;
 
-	product : Product;
+	product : Product = null;
 
-	constructor(private productCommunicationService : ProductCommunicationService, private  imageService : ImageService) { }
+	//ChangeDetector is used to find image el after view has been updated after product came up for the first time.
+	constructor(private productCommunicationService : ProductCommunicationService, private imageService : ImageService, private cdRef:ChangeDetectorRef) { }
 
 	ngOnInit() {
-		
+		this.subscription =	this.productCommunicationService.channel$.subscribe(p => this.setProduct(p));
 	}
 
-	setSelectedProduct(p: Product){
+	setProduct(p : Product){
 		this.product = p;
-		this.source = this.imageService.getImageSrcById(this.product.imageId);
-	//	this.el.nativeElement.src = this.source;
-		console.log(this.source);
+		this.cdRef.detectChanges();
+		this.el.nativeElement.src =  this.imageService.getImageSrcById(this.product.imageId);
 	}
-
-	ngAfterViewInit(): void {
-		this.subscription =	this.productCommunicationService.channel$.subscribe( p => this.setSelectedProduct(p));
-
-	}
-
 
 	ngOnDestroy(){
 		this.subscription.unsubscribe();
